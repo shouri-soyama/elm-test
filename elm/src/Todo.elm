1091f7d1c4ba5fa48068a45cedd45ceba6d0060b
S.Soyama <s_soyama@denkiyagi.jp>
2015-07-22 16:14:23 +0900

    todoリストへのポストを可能にした

--- elm/src/Todo.elm
+++ elm/src/Todo.elm
@@ -2,28 +2,71 @@ module Todo where
 
 import Html as H
 import Html.Attributes as HA
+import Html.Events as HE
+import Html.Lazy as HL
+import Signal as S
 import StartApp
 
-main : Signal Html
+main : Signal H.Html
 main =
   StartApp.start { model = model, view = view, update = update }
 
-type alias Model = List String 
+type alias Model = 
+  { form : String
+  , posts : List String
+  }
 
 model : Model
-model = []
+model = 
+  { form = ""
+  , posts = []
+  }
 
-view : Address action -> Model -> Html
+view : S.Address Action -> Model -> H.Html
 view address model = 
-  H.div [ HA.class "row" ]
-  [ H.input [] []
-  , H.button [] []
+  H.div []
+  [ H.div [ HA.class "row" ]
+    [ H.div [ HA.class "col-md-8" ]
+      [ H.input 
+        [ HA.type' "text"
+        , HA.class "form-control"
+        , HE.on "input" HE.targetValue (S.message address << Change)
+        ] []
+      ]
+      , H.div [ HA.class "col-md-4" ]
+        [ H.button 
+          [ HA.type' "button"
+          , HA.class "btn btn-default"
+          , HE.onClick address Post
+        ] [ H.text "submit" ]
+      ]
+    ]
+  , H.div [ HA.class "row" ]
+    [ HL.lazy list model.posts ]
   ]
 
-type Action = Post String
+list : List String -> H.Html
+list posts =
+  let
+    toTr post = H.tr []
+      [ H.td [] [ H.text (post) ]
+      ]
+  in
+    H.table
+      [ HA.class "table" ]
+      [ H.thead []
+        [ H.th [ HA.style [ ("width", "100px") ] ] [ H.text "todo" ]
+        ]
+      , H.tbody [] <| List.map toTr posts
+      ]
+
+type Action
+  = Change String
+  | Post
 
 update : Action -> Model -> Model
 update action model =
   case action of
-    Post str -> str :: model
+    Change str -> { model | form <- str }
+    Post -> { model | posts <- model.form :: model.posts }
 
